39d829f9a218c53f6085a19aace119b6612dfd7f
Mahdi Yusuf <it.mahdi.yusuf@gmail.com>
2019-05-12 19:30:29 +0700

    Add video example

--- src/counter/runScript.sh
+++ src/counter/runScript.sh
@@ -1,3 +1,4 @@
 python /home/oracl4/project/hitungin/src/counter/people_counter.py --prototxt /home/oracl4/project/hitungin/src/counter/mobilenet_ssd/MobileNetSSD_deploy.prototxt \
 	--model /home/oracl4/project/hitungin/src/counter/mobilenet_ssd/MobileNetSSD_deploy.caffemodel \
-	--input /home/oracl4/project/hitungin/input/input.mp4 
+	--input /home/oracl4/project/hitungin/input/input2.m4v \
+	--output /home/oracl4/project/hitungin/output/output2.avi
